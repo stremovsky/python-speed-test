cb2ce7a7a1b71034cde91f0726fa5d40fcdd750b
Ubuntu <ubuntu@ip-172-21-3-198.us-west-1.compute.internal>
2023-12-08 13:04:31 +0000

    fix url

--- japronto-hello-world/hello.py
+++ japronto-hello-world/hello.py
@@ -4,5 +4,5 @@ def hello(request):
     return request.Response(text='Hello world!')
 
 app = Application()
-app.router.add_route('/', hello)
+app.router.add_route('/hello', hello)
 app.run(host='0.0.0.0', port=8080, debug=False)
