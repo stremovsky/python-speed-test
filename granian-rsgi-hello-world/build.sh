aa8d97127231ce5d9818465d500a4b1d03908875
Ubuntu <ubuntu@ip-172-21-3-198.us-west-1.compute.internal>
2023-12-08 10:20:35 +0000

    add new tests

---
+++ granian-rsgi-hello-world/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t granian-rsgi-hello-world .
---
+++ granian-rsgi-hello-world/hello.py
@@ -0,0 +1,10 @@
+async def app(scope, proto):
+    assert scope.proto == 'http'
+
+    proto.response_str(
+        status=200,
+        headers=[
+            ('content-type', 'text/plain')
+        ],
+        body="Hello, world!"
+    )
