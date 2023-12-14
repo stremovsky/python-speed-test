e2631a74ad992d5de3d9273d78c90746dfc55966
Ubuntu <ubuntu@ip-172-21-3-198.us-west-1.compute.internal>
2023-12-14 16:16:04 +0000

    enable HTTP/1.1 protocol

--- flask-hello-world/hello.py
+++ flask-hello-world/hello.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from werkzeug.serving import WSGIRequestHandler
 
+#enable HTTP/1.1 for Keep-Alive
+WSGIRequestHandler.protocol_version = "HTTP/1.1"
 app = Flask(__name__)
 
 @app.route('/hello')
